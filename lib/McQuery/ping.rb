92fcd64f972a32388bc39cd86e296c35bcb4a0cf
ryush00 <ryush00@gmail.com>
2016-02-29 06:53:14 +0000

    Fix bug with protocolsupport

--- lib/McQuery/ping.rb
+++ lib/McQuery/ping.rb
@@ -1,29 +1,61 @@
 require 'socket'
 
 module McQuery
-  class Ping
-    attr_reader :protocol_version, :server_version, :motd, :players_online, :players_max
-    def initialize(hostname, port)
-      @hostname = hostname
-      @port = port
-      doPing
-    end
+    class Ping
+        def initialize(host, port=25565)
+            @host = host
+            @port = port
+        end
 
-    private
-    def doPing
-      s = TCPSocket.open(@hostname, @port)
+        def ping
+            socket = TCPSocket.open(@host, @port)
+            # packet identifier & payload ...
+            socket.write([0xFE, 0x01, 0xFA].pack('CCC'))
+            socket.write(encode_string('MC|PingHost'))
+            socket.write([7 + 2 * @host.length].pack('n'))
+            socket.write([74].pack('c'))
+            socket.write(encode_string(@host))
+            socket.write([@port].pack('N'))
 
-      s.puts "\xFE\x01"
-      repl = s.read
-      s.close
-      qstring = repl[3,repl.length].force_encoding("utf-16be").encode("utf-8")
-      qarray = qstring.split("\0")
-      qdict = {}
-      @protocol_version = qarray[1]
-      @server_version = qarray[2]
-      @motd = qarray[3]
-      @players_online = qarray[4]
-      @players_max = qarray[5]
-    end
+            # read server response
+            if socket.read(1).unpack('C').first != 0xFF # Kick packet
+                raise 'unexpected server response packet'
+            end
+
+            len = socket.read(2).unpack('n').first
+            resp = decode_string(socket.read(len*2)).split("\u0000")
+
+            socket.close
+
+            if resp.shift != "\u00A71"
+                raise 'unexpected server response fields'
+            end
+
+            return {
+                :protocol_version => resp.shift.to_i,
+                :server_version => resp.shift,
+                :motd => resp.shift,
+                :players_online => resp.shift.to_i,
+                :players_max => resp.shift.to_i
+            }
+        end
+
+        private
+
+        def encode_string(s)
+            begin
+                [s.length].pack('n') + Iconv.conv('utf-16be', 'utf-8', s)
+            rescue
+                [s.length].pack('n') + s.encode('utf-16be').force_encoding('ASCII-8BIT')
+            end
+        end
+
+        def decode_string(s)
+            begin
+                Iconv.conv('utf-8', 'utf-16be', s)
+            rescue
+                s.force_encoding('utf-16be').encode('utf-8')
+            end
+        end
   end
 end
